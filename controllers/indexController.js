0ec731bf99aa63851b54f522b92120fc97b44669
Kartohan <Kartohan@gmail.com>
2022-10-24 17:31:06 +0300

    Create templates

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,7 @@
 exports.index = function (req, res) {
   res.send("Not implemented Index Page");
 };
+
+exports.templates = function (req, res) {
+  res.render("templates");
+};
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const indexController = require("../controllers/indexController");
 
 /* GET home page. */
 router.get("/", indexController.index);
+router.get("/templates", indexController.templates);
 
 module.exports = router;
