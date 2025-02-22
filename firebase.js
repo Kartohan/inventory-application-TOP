35ff33ebe2537b854506daec79b9dff29d66e97e
Kartohan <Kartohan@gmail.com>
2025-02-23 01:04:22 +0200

    Platform change

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("/etc/secrets/serviceAccountKey.json");
 const sharp = require("sharp");
 
 admin.initializeApp({
